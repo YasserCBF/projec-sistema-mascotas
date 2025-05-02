69bb5fde6ddf39aae8392de3a965cfd75ec790ad
YasserCBF <1520478@senati.pe>
2025-05-02 07:51:33 -0500

    modificacion de los archivos de react/frontend

--- pet-care-frontend/src/App.js
+++ pet-care-frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Login from './components/Login';
+import PetCard from './components/PetCard';
+import BookingForm from './components/BookingForm';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ textAlign: 'center', color: 'var(--gris-oscuro)' }}>Sistema de Cuidado de Mascotas</h1>
+      <Login />
+      <PetCard />
+      <BookingForm />
     </div>
   );
 }
